d311c267d6b5d3907b5131c1036c6d4bf5e254e9
Raf <r0463108@student.thomasmore.be>
2022-10-19 11:02:05 +0200

    Edited sendmail.php
    Rewrote mail to be sent in html for easier layout styling.

--- Les 04 - Forms/sendmail.php
+++ Les 04 - Forms/sendmail.php
@@ -1,16 +1,19 @@
 <?php
 $studentemail = htmlspecialchars($_POST['studentemail']);
 $subject = empty(htmlspecialchars($_POST['subject'])) ? "No subject" : htmlspecialchars($_POST['subject']);
-$message = empty(htmlspecialchars($_POST['message'])) ? "Default message" : htmlspecialchars($_POST['message']);
+$message = '<html><body><table border="2"><tr><th>Message</th><td>';
+$message .= empty(htmlspecialchars($_POST['message'])) ? "Default message" : htmlspecialchars($_POST['message']);
+$message .= "</td></tr>";
 if (!empty($_POST['random'])) {
-    $message .= " \r\n " . htmlspecialchars($_POST['random']);
+    $message .= "<tr><th>Random</th><td>" . htmlspecialchars($_POST['random']) . "</td></tr>";
 }
 if (!empty($_POST['date'])) {
-    $message .= " \r\n " . htmlspecialchars($_POST['date']);
+    $message .= "<tr><th>Date</th><td>" . htmlspecialchars($_POST['date']) . "</td></tr>";
 }
 if (!empty($_POST['number'])) {
-    $message .= " \r\n " . htmlspecialchars($_POST['number']);
+    $message .= "<tr><th>Number</th><td>" . htmlspecialchars($_POST['number']) . "</td></tr>";
 }
+$message .= '</table>';
 
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
